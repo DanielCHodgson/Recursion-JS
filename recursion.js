8ab6dc64448e6e61f55c3ab40b7da979c4030983
Dan H <daniel.c.hodgson@address.com>
2025-10-24 10:51:22 +0100

    Fibonacci solved recursively

--- recursion.js
+++ recursion.js
@@ -13,3 +13,19 @@ function fibs(seq) {
 }
 
 console.log(fibs(12));
+
+function fibsrec(seq) {
+  //console.log("This was printed recursively: " + seq);
+  if (seq <= 0) {
+    return [0];
+  } else if (seq === 2) {
+    return [0, 1];
+  }
+
+  const array = fibsrec(seq - 1);
+  //console.log(seq + ": " + array);
+  array.push(array[array.length - 1] + array[array.length - 2]);
+  return array;
+}
+
+console.log(fibsrec(12));
